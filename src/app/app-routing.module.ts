97d5fccd53cca934e542b1e48da7606cb8911b03
James Slater <thomas.james.slater@gmail.com>
2018-09-26 10:55:39 -0700

    More Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {RothkoComponent} from './rothko/rothko.component';
+import {AppComponent} from './app.component';
+
 
 
 const routes: Routes = [
-  {
-    path: 'rothko', component: RothkoComponent
-  }
+  { path: 'rothko', component: RothkoComponent },
+  { path: '', redirectTo: '/rothko', pathMatch: 'full' },
 ];
 
 
